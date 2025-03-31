7cfbf499b4678c1f378af9e56d3f6a54aab6344e
Vikram <kariyavulavikram2300@gmail.com>
2025-03-31 16:02:01 +0530

    test

--- utility_services/settings.py
+++ utility_services/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
